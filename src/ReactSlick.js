05cde13efc389f2ba9652fea1390a014bba72ecf
Kabzhan Shynghys <shynghys@mail.ru>
2021-03-11 18:24:29 +0600

    ss

---
+++ src/ReactSlick.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+
+import Slider from "react-slick";
+import "./index.css";
+
+
+
+const ReactSlick = () => {
+    const [animals, setAnimals] = useState([{ src: 'http://placekitten.com/g/400/400', type: 'cat' }, { src: 'http://placekitten.com/g/800/400', type: 'cat' }, { src: 'http://placekitten.com/g/1400/1200', type: 'cat' }, { src: 'http://placekitten.com/g/800/800', type: 'cat' }, { src: 'https://placedog.net/500/280', type: 'dog' }, { src: 'https://placedog.net/600/380', type: 'dog' }])
+
+    const arrayToCats = (animals) => {
+        this.setAnimals(animals.filter(animal => animal.type === 'cat'))
+
+    }
+    var settings = {
+        dots: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        speed: 1000,
+        autoplaySpeed: 4000,
+        cssEase: "linear",
+        pauseOnHover: true,
+        infinite: true,
+    };
+
+
+
+    return (
+        <div>
+            <div className="container">
+                <Slider {...settings}>
+                    {animals.map((animal) =>
+                        <div >
+                            <img src={animal.src} alt={animal.type} />
+                        </div>
+                    )}
+                </Slider>
+
+            </div>
+            {/* onClick={ } */}
+            <button onClick={arrayToCats(animals)}>Only cats!</button>
+        </div >
+    );
+}
+
+
+
+export default ReactSlick
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import ReactSlick from './ReactSlick';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ReactSlick />
+  </React.StrictMode>,
+  document.getElementById('container')
+);
