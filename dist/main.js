41ff04150a082963c5494f93f85738fc88f5348b
Kabzhan Shynghys <shynghys@mail.ru>
2021-03-14 19:43:28 +0600

    class

---
+++ dist/main.js
@@ -0,0 +1,37 @@
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else if(typeof exports === 'object')
+		exports["Slider"] = factory();
+	else
+		root["Slider"] = factory();
+})(self, function() {
+return /******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nSyntaxError: Unexpected token (9:2)\n\n\u001b[0m \u001b[90m  7 | \u001b[39m\n \u001b[90m  8 | \u001b[39m\u001b[33mReactDOM\u001b[39m\u001b[33m.\u001b[39mrender(\n\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m  9 | \u001b[39m  \u001b[33m<\u001b[39m\u001b[33mReact\u001b[39m\u001b[33m.\u001b[39m\u001b[33mStrictMode\u001b[39m\u001b[33m>\u001b[39m\n \u001b[90m    | \u001b[39m  \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\n \u001b[90m 10 | \u001b[39m    \u001b[33m<\u001b[39m\u001b[33mReactSlick\u001b[39m \u001b[33m/\u001b[39m\u001b[33m>\u001b[39m\n \u001b[90m 11 | \u001b[39m  \u001b[33m<\u001b[39m\u001b[33m/\u001b[39m\u001b[33mReact\u001b[39m\u001b[33m.\u001b[39m\u001b[33mStrictMode\u001b[39m \u001b[33m>\u001b[39m\u001b[33m,\u001b[39m\n \u001b[90m 12 | \u001b[39m  document\u001b[33m.\u001b[39mgetElementById(\u001b[32m'container'\u001b[39m)\u001b[0m\n");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ 	return __webpack_exports__;
+/******/ })()
+;
+});
+//# sourceMappingURL=main.js.map
--- src/index.js
+++ src/index.js
@@ -1,11 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import ReactSlick from './ReactSlick';
+import Slider from "react-slick";
+
+import "./index.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+class ReactSlick extends React.Component {
+  constructor(props) {
+    super(props);
+
+    const [animals, setAnimals] = useState([])
+    this.state = {
+      animals: [{ src: 'http://placekitten.com/g/400/400', type: 'cat' }, { src: 'http://placekitten.com/g/800/400', type: 'cat' }, { src: 'http://placekitten.com/g/1400/1200', type: 'cat' }, { src: 'http://placekitten.com/g/800/800', type: 'cat' }, { src: 'https://placedog.net/500/280', type: 'dog' }, { src: 'https://placedog.net/600/380', type: 'dog' }],
+      settings: {
+        dots: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        speed: 1000,
+        autoplaySpeed: 4000,
+        cssEase: "linear",
+        pauseOnHover: true,
+        infinite: true,
+      }
+    }
+
+  }
+
+  arrayToCats(animals) {
+    this.setAnimals(animals.filter(animal => animal.type === 'cat'))
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="container">
+          <Slider {...this.settings}>
+            {this.animals.map((animal) =>
+              <div >
+                <img src={animal.src} alt={animal.type} />
+              </div>
+            )}
+          </Slider>
+        </div>
+        <button onClick={this.arrayToCats(this.animals)}>Only cats!</button>
+      </div >
+    );
+  }
+}
 
 ReactDOM.render(
   <React.StrictMode>
     <ReactSlick />
-  </React.StrictMode>,
+  </React.StrictMode >,
   document.getElementById('container')
 );
